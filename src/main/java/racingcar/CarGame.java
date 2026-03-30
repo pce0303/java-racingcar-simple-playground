4b324f02a3a7d2169673c5ec705a883c8c84df01
pce0303 <ivychoen@gmail.com>
2026-03-30 10:01:30 +0900

    refactor: 1차 리뷰 반영

--- src/main/java/racingcar/CarGame.java
+++ src/main/java/racingcar/CarGame.java
@@ -16,8 +16,8 @@ public class CarGame {
 
     public void racing(int n) {
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < cars.size(); j++) {
-                cars.get(j).addDistance();
+            for (Car car : cars) {
+                car.addDistance();
             }
         }
     }
