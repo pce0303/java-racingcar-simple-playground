713b1b11097b023f4653d0e8ca3e340706130b04
pce0303 <ivychoen@gmail.com>
2026-03-30 14:11:32 +0900

    feat: 입력 클래스 구현

---
+++ src/main/java/racingcar/View.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class View {
+    Scanner scanner = new Scanner(System.in);
+
+    public List<String> inputName() {
+        String fullName = scanner.nextLine();
+        List<String> names = List.of(fullName.split(","));
+
+        for (String name : names) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("이름은 5자 이하여야 합니다.");
+            }
+        }
+
+        return names;
+    }
+
+    public int inputTimes() {
+        return scanner.nextInt();
+    }
+}
