ba0924e133f51d9a27b76cd76a02ca2b78937f11
pce0303 <ivychoen@gmail.com>
2026-03-30 13:21:59 +0900

    feat: 2단계 테스트 구현

--- src/test/java/racingcar/CarGameTest.java
+++ src/test/java/racingcar/CarGameTest.java
@@ -1,5 +1,58 @@
 package racingcar;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import java.util.Random;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class CarGameTest {
 
+    Random Move = new Random() {
+        @Override
+        public int nextInt(int bound) {
+            return 4;
+        }
+    };
+    Random Stop = new Random() {
+        @Override
+        public int nextInt(int bound) {
+            return 2;
+        }
+    };
+
+    @Test
+    @DisplayName("모든 차가 똑같이 전진할 때")
+    public void allMove() {
+        CarGame carGame = new CarGame();
+        carGame.createCar(List.of("choeun", "sieun", "sanghun"), List.of(Move, Move, Move));
+
+        carGame.racing(5);
+
+        List<String> winner = carGame.winner();
+        assertThat(winner).contains("choeun", "sieun", "sanghun");
+    }
+
+    @Test
+    @DisplayName("한 대만 전진하고 나머지는 중간에 멈출 때")
+    public void oneWinner() {
+        CarGame carGame = new CarGame();
+        carGame.createCar(List.of("choeun", "sieun", "sanghun"), List.of(Move, Stop, Stop));
+        carGame.racing(5);
+
+        List<String> winner = carGame.winner();
+        assertThat(winner).contains("choeun");
+    }
+
+    @Test
+    @DisplayName("모든 차가 멈출 때")
+    public void allStop() {
+        CarGame carGame = new CarGame();
+        carGame.createCar(List.of("choeun", "sieun", "sanghun"), List.of(Stop, Stop, Stop));
+        carGame.racing(5);
+
+        assertThatThrownBy(carGame::winner).isInstanceOf(IllegalStateException.class);
+    }
 }
